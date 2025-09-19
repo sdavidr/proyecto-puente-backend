62a35a2047101c5714abf31a153783e83501bc0f
sdavidr <mutabimobile@gmail.com>
2025-09-19 11:51:48 +0200

    New class User

---
+++ src/main/java/com/mutabi/model/User.java
@@ -0,0 +1,21 @@
+package com.mutabi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+
+
+}
